387e6227892506abd04e6e228b8f846c1cebd3d6
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-08 13:00:17 -0400

    feat: Created drop sql script for dropping entire database HA

---
+++ database/drop_HA.sql
@@ -0,0 +1,134 @@
+-- DROP Telefonos, Correos
+DROP TABLE Telefono;
+
+DROP TABLE Correo;
+
+-- DROP Caracteristicas de la cerveza
+DROP TABLE RECE_INGR;
+
+DROP TABLE CERV_CARA;
+
+DROP TABLE TIPO_CARA;
+
+DROP TABLE Caracteristica;
+
+DROP TABLE Ingrediente;
+
+DROP TABLE Instruccion;
+
+-- DROP Usuarios
+DROP TABLE Usuario;
+
+DROP TABLE PRIV_ROL;
+
+DROP TABLE Rol;
+
+DROP TABLE Privilegio;
+
+-- Drop Empleados
+DROP TABLE Asistencia;
+
+DROP TABLE Vacacion;
+
+DROP TABLE EMPL_BENE;
+
+DROP TABLE Beneficio;
+
+DROP TABLE EMPL_HORA;
+
+DROP TABLE Horario;
+
+DROP TABLE EMPL_CARG;
+
+DROP TABLE Cargo;
+
+DROP TABLE Departamento;
+
+DROP TABLE Empleado;
+
+-- DROP Estado
+DROP TABLE ESTA_EVEN;
+
+DROP TABLE ESTA_COMP;
+
+DROP TABLE ESTA_VENT;
+
+DROP TABLE Estatus;
+
+-- DROP Ventas
+DROP TABLE PUNT_CLIE;
+
+DROP TABLE Pago;
+
+DROP TABLE Detalle_Venta;
+
+DROP TABLE Venta;
+
+DROP TABLE Tasa;
+
+DROP TABLE Cuota;
+
+-- DROP Compras
+DROP TABLE Detalle_Compra;
+
+DROP TABLE Compra;
+
+-- DROP Inventarios - Tiendas - Eventos
+DROP TABLE Inventario_Tienda;
+
+DROP TABLE Inventario_Evento;
+
+DROP TABLE Tienda;
+
+DROP TABLE Lugar_Tienda;
+
+DROP TABLE Registro_Evento;
+
+DROP TABLE Evento;
+
+DROP TABLE Tipo_Evento;
+
+DROP TABLE Juez;
+
+-- DROP Cervezas
+DROP TABLE DESC_CERV;
+
+DROP TABLE Descuento;
+
+DROP TABLE CERV_PRES;
+
+DROP TABLE Presentacion;
+
+DROP TABLE Cerveza;
+
+DROP TABLE Tipo_Cerveza;
+
+DROP TABLE Receta;
+
+-- DROP Metodos de Pago
+DROP TABLE Efectivo;
+
+DROP TABLE Punto_Canjeo;
+
+DROP TABLE Tarjeta;
+
+DROP TABLE Cheque;
+
+DROP TABLE Banco;
+
+DROP TABLE Metodo_Pago;
+
+-- Drop Clientes
+DROP TABLE Cliente_Natural;
+
+DROP TABLE Cliente_Juridico;
+
+DROP TABLE Cliente;
+
+-- Drop Miembros
+DROP TABLE Contacto;
+
+DROP TABLE Miembro;
+
+-- DROP Lugar
+DROP TABLE Lugar;
