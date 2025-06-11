7c709a51408bb45d0f2f6c67a493f2f2421d7b1a
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-11 16:02:40 -0400

    feat: Improved logging

--- backend/src/delete.ts
+++ backend/src/delete.ts
@@ -2,7 +2,7 @@ import { sql } from "bun";
 import Logger from "./logger/logger";
 
 async function quickDelete(req: Bun.BunRequest): Promise<Response> {
-	Logger.info(`Insert request incoming from ${req.url}`)
+	Logger.info(`Insert request incoming ${req.method}:${req.url}`)
 	const body: any = await req.json();
 
 	const table: string = body.table;
@@ -16,11 +16,11 @@ async function quickDelete(req: Bun.BunRequest): Promise<Response> {
 		Logger.debug(`SQL Query result: ${res}`)
 		return Response.json(res);
 	} catch (e: unknown) {
-		let response: string = ''
+		let response: string = 'quickDelete() -> '
 		if (typeof e === 'string')
-			response = `An error has occurred: ${e}`
+			response += `An error has occurred: ${e}`
 		else if (e instanceof Error)
-			response = `An error has occurred: ${e.message}`
+			response += `An error has occurred: ${e.message}`
 		Logger.error(response)
 		return new Response(response, {
 			status: 400,
