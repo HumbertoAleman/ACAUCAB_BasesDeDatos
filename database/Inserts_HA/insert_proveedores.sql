cd18057eff225c10e2b91038e01f27eb02a3df02
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-11 17:57:31 -0400

    fix: Fixed name of add_miembro procedure

--- database/Inserts_HA/insert_proveedores.sql
+++ database/Inserts_HA/insert_proveedores.sql
@@ -1,5 +1,5 @@
 -- Generated Using https://www.coderstool.com/sql-test-data-generator
-CREATE OR REPLACE PROCEDURE add_employee (rif text, razon_social text, denom_comercial text, direccion_fiscal text, pag_web text, luga_1 integer, luga_2 integer)
+CREATE OR REPLACE PROCEDURE add_miembro (rif text, razon_social text, denom_comercial text, direccion_fiscal text, pag_web text, luga_1 integer, luga_2 integer)
 LANGUAGE plpgsql
 AS $$
 DECLARE
@@ -25,45 +25,45 @@ DELETE FROM Miembro;
 INSERT INTO Rol
     VALUES (100, 'Miembro', 'Proveedor de cerveza, les compramos');
 
-CALL add_employee ('J-66346', 'Blacks International ', 'Interior Mutual S.A', '6287 Holmcroft, Simi Valley, Rhode Island, 54650', 'https://crowd.nationwide/ver.php', 1, 1);
+CALL add_miembro ('J-66346', 'Blacks International ', 'Interior Mutual S.A', '6287 Holmcroft, Simi Valley, Rhode Island, 54650', 'https://crowd.nationwide/ver.php', 1, 1);
 
-CALL add_employee ('J-69163', 'Gulf Corp', 'Stewart ', '8208 Melland Avenue, Fort Worth, Indiana, 05019', 'https://www.innocent.com/vienna.html', 1, 1);
+CALL add_miembro ('J-69163', 'Gulf Corp', 'Stewart ', '8208 Melland Avenue, Fort Worth, Indiana, 05019', 'https://www.innocent.com/vienna.html', 1, 1);
 
-CALL add_employee ('J-08316', 'Closes International Company', 'Invest Stores Corp', '2639 Mill, Richmond County, New York, 45183', 'http://simpson.com/positive', 1, 1);
+CALL add_miembro ('J-08316', 'Closes International Company', 'Invest Stores Corp', '2639 Mill, Richmond County, New York, 45183', 'http://simpson.com/positive', 1, 1);
 
-CALL add_employee ('J-39048', 'Recording Corporation', 'Transferred ', '2612 Maynestone Road, Leominster, Louisiana, 82578', 'https://bargain.com/involve.php', 1, 1);
+CALL add_miembro ('J-39048', 'Recording Corporation', 'Transferred ', '2612 Maynestone Road, Leominster, Louisiana, 82578', 'https://bargain.com/involve.php', 1, 1);
 
-CALL add_employee ('J-37802', 'Animation Stores SIA', 'Liver B.V', '1885 Sherbrooke Street, Spokane, Ohio, 23210', 'https://www.despite.com/japanese.aspx', 1, 1);
+CALL add_miembro ('J-37802', 'Animation Stores SIA', 'Liver B.V', '1885 Sherbrooke Street, Spokane, Ohio, 23210', 'https://www.despite.com/japanese.aspx', 1, 1);
 
-CALL add_employee ('J-59401', 'Brooks ', 'Largely Holdings Company', '9875 Light Circle, Brighton, Montana, 87266', 'https://www.historical.com/mumbai.aspx', 1, 1);
+CALL add_miembro ('J-59401', 'Brooks ', 'Largely Holdings Company', '9875 Light Circle, Brighton, Montana, 87266', 'https://www.historical.com/mumbai.aspx', 1, 1);
 
-CALL add_employee ('J-64201', 'Professional Company', 'Equilibrium International SIA', '7220 Tedder, Miramar, Indiana, 73039', 'https://xerox.com/portuguese.aspx#genuine', 1, 1);
+CALL add_miembro ('J-64201', 'Professional Company', 'Equilibrium International SIA', '7220 Tedder, Miramar, Indiana, 73039', 'https://xerox.com/portuguese.aspx#genuine', 1, 1);
 
-CALL add_employee ('J-27169', 'Clear Energy Corporation', 'Sara LLC', '6027 Radclyffe Circle, Kenosha, Iowa, 91275', 'https://lexington.com/peter.php#ones', 1, 1);
+CALL add_miembro ('J-27169', 'Clear Energy Corporation', 'Sara LLC', '6027 Radclyffe Circle, Kenosha, Iowa, 91275', 'https://lexington.com/peter.php#ones', 1, 1);
 
-CALL add_employee ('J-62556', 'Commander Industries A.G', 'Shaft Stores Corporation', '0635 Pelham Circle, Henderson, Delaware, 25851', 'https://www.employees.com/angry.php', 1, 1);
+CALL add_miembro ('J-62556', 'Commander Industries A.G', 'Shaft Stores Corporation', '0635 Pelham Circle, Henderson, Delaware, 25851', 'https://www.employees.com/angry.php', 1, 1);
 
-CALL add_employee ('J-65703', 'Pete Mutual GmbH', 'Pearl International Corporation', '3457 Cardus Avenue, Sacramento, Nebraska, 54606', 'https://www.money.com/antonio', 1, 1);
+CALL add_miembro ('J-65703', 'Pete Mutual GmbH', 'Pearl International Corporation', '3457 Cardus Avenue, Sacramento, Nebraska, 54606', 'https://www.money.com/antonio', 1, 1);
 
-CALL add_employee ('J-64464', 'Tail Stores SIA', 'International Software Inc', '6904 Talkin Circle, Overland Park, Alaska, 44352', 'http://www.occupational.com/vernon', 1, 1);
+CALL add_miembro ('J-64464', 'Tail Stores SIA', 'International Software Inc', '6904 Talkin Circle, Overland Park, Alaska, 44352', 'http://www.occupational.com/vernon', 1, 1);
 
-CALL add_employee ('J-08283', 'Agreements SIA', 'Repository Holdings ', '9571 Caldecott, Detroit, Kansas, 48806', 'http://nebraska.com/responded.aspx', 1, 1);
+CALL add_miembro ('J-08283', 'Agreements SIA', 'Repository Holdings ', '9571 Caldecott, Detroit, Kansas, 48806', 'http://nebraska.com/responded.aspx', 1, 1);
 
-CALL add_employee ('J-38915', 'Fog Industries GmbH', 'Designing Mutual S.A', '9500 Coke, Pittsburgh, Tennessee, 46878', 'https://globe.com/atlanta.aspx', 1, 1);
+CALL add_miembro ('J-38915', 'Fog Industries GmbH', 'Designing Mutual S.A', '9500 Coke, Pittsburgh, Tennessee, 46878', 'https://globe.com/atlanta.aspx', 1, 1);
 
-CALL add_employee ('J-57872', 'Sold Industries S.A', 'Associates Industries ', '3553 Stonepail Road, Tempe, Alabama, 53812', 'https://automobile.com/circus.html', 1, 1);
+CALL add_miembro ('J-57872', 'Sold Industries S.A', 'Associates Industries ', '3553 Stonepail Road, Tempe, Alabama, 53812', 'https://automobile.com/circus.html', 1, 1);
 
-CALL add_employee ('J-09215', 'Itunes International ', 'Earlier LLC', '5637 Beech Street, Newburgh, Georgia, 52735', 'http://www.isolation.manchester.museum/revelation', 1, 1);
+CALL add_miembro ('J-09215', 'Itunes International ', 'Earlier LLC', '5637 Beech Street, Newburgh, Georgia, 52735', 'http://www.isolation.manchester.museum/revelation', 1, 1);
 
-CALL add_employee ('J-50018', 'Syndication Mutual S.A', 'Solar Holdings Corp', '0717 Wincombe Avenue, Ogden, Nebraska, 31435', 'https://www.myrtle.freebox-os.com/suspect.php', 1, 1);
+CALL add_miembro ('J-50018', 'Syndication Mutual S.A', 'Solar Holdings Corp', '0717 Wincombe Avenue, Ogden, Nebraska, 31435', 'https://www.myrtle.freebox-os.com/suspect.php', 1, 1);
 
-CALL add_employee ('J-57937', 'Gpl Stores LLC', 'Caution Holdings GmbH', '1009 Thirlwater Road, Anaheim, Rhode Island, 34589', 'https://instant.com/currencies.aspx', 1, 1);
+CALL add_miembro ('J-57937', 'Gpl Stores LLC', 'Caution Holdings GmbH', '1009 Thirlwater Road, Anaheim, Rhode Island, 34589', 'https://instant.com/currencies.aspx', 1, 1);
 
-CALL add_employee ('J-60693', 'Kernel Energy ', 'Peru B.V', '5593 Hollinhurst Lane, Tulsa, Rhode Island, 72951', 'http://conducting.com/mess.php', 1, 1);
+CALL add_miembro ('J-60693', 'Kernel Energy ', 'Peru B.V', '5593 Hollinhurst Lane, Tulsa, Rhode Island, 72951', 'http://conducting.com/mess.php', 1, 1);
 
-CALL add_employee ('J-26976', 'Upc Industries SIA', 'Ending International S.A', '9767 Raydon Avenue, Provo, Florida, 56944', 'https://www.navigator.bømlo.no/lucy', 1, 1);
+CALL add_miembro ('J-26976', 'Upc Industries SIA', 'Ending International S.A', '9767 Raydon Avenue, Provo, Florida, 56944', 'https://www.navigator.bømlo.no/lucy', 1, 1);
 
-CALL add_employee ('J-08025', 'Coordinate Industries Company', 'Emotions International ', '9059 Reddisher Circle, Grand Prairie, Maine, 73188', 'http://condos.com/rock.aspx', 1, 1);
+CALL add_miembro ('J-08025', 'Coordinate Industries Company', 'Emotions International ', '9059 Reddisher Circle, Grand Prairie, Maine, 73188', 'http://condos.com/rock.aspx', 1, 1);
 
 SELECT
     *
