38f7f6428f3ea77f4aab72ee5805fc70dbec7b54
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-12 12:03:26 -0400

    feat: Created INSERT for Tipo_Evento

---
+++ database/Inserts_HA/insert_tipo_evento.sql
@@ -0,0 +1,31 @@
+-- Generated type of events using duck.ai GTP-o4 mini with the following prompt
+-- I have a beer company, what are 10 different type of events that I can insert into my database
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Cata de cervezas');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Recorrido por cerveceria');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Festival de cerveza');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Cena de maridaje');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Taller de elaboración de cerveza');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Fiesta de lanzamiento de cervezas de temporada');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Noche de música en vivo');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Noche de trivia');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Evento de cerveza y arte');
+
+INSERT INTO Tipo_Evento (nombre_tipo_even)
+    VALUES ('Evento de networking para cerveceros');
