9271eafb232d76e20ad2929fa5781b68caaf5aeb
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-12 11:54:34 -0400

    feat: Created INSERT for Juez

---
+++ database/Inserts_HA/insert_juez.sql
@@ -0,0 +1,31 @@
+-- Generated Using https://mockaroo.com/
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Dot', 'Bay', 'Linton', 'Dossit', '0785098');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Patton', 'Grayce', 'Bartlosz', 'Roston', '7272681');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Quincy', 'Renee', 'Krzysztofiak', 'Pietz', '8088538');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Janeva', 'Tera', 'Belz', 'Amis', '5472356');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Valle', 'Valle', 'Trevarthen', 'Jadczak', '6944297');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Ephrem', 'Sybilla', 'O''Cannovane', 'Indgs', '4532104');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Albertina', 'Ermentrude', 'Linnell', 'Fraczek', '3006741');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Georgine', 'Haskel', 'Reeks', 'Regi', '9667987');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Nora', 'Gayla', 'Ensten', 'Jeandeau', '0912550');
+
+INSERT INTO Juez (primar_nom_juez, segundo_nom_juez, primar_ape_juez, segundo_ape_juez, ci_juez)
+    VALUES ('Marwin', 'Llywellyn', 'Fairney', 'Brain', '0908301');
