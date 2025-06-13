c53df100d1f399e2a2da1985055ea31c2dc99cea
Eiderwoss <oscarmanrique.sta@gmail.com>
2025-06-13 00:27:45 -0400

    fix + feat: Added functions that return a random cliente/miembro/contacto. Also fixed de code because SELECT can't be used as an argument in a CALL

--- database/Inserts_OM/insert_telefono.sql
+++ database/Inserts_OM/insert_telefono.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE add_tlf (cod_area integer, numero integer, cliente varchar(20), contacto integer, miembro varchar(20))
+CREATE OR REPLACE PROCEDURE add_tlf (cod_area integer, numero integer, cliente varchar(20), contacto integer, miembro varchar(20))
 LANGUAGE plpgsql
 AS $$
 BEGIN
@@ -7,13 +7,58 @@ BEGIN
 END
 $$
 
-CALL add_tlf(0412, 9876543, NULL, SELECT cod_pers FROM Contacto ORDER BY RANDOM() LIMIT 1, NULL);
-CALL add_tlf(0414, 1234567, SELECT rif_clie FROM Cliente ORDER BY RANDOM() LIMIT 1, NULL, NULL);
-CALL add_tlf(0424, 7654321, NULL, NULL, SELECT rif_miem FROM Miembro ORDER BY RANDOM() LIMIT 1);
-CALL add_tlf(0416, 2345678, NULL, SELECT cod_pers FROM Contacto ORDER BY RANDOM() LIMIT 1, NULL);
-CALL add_tlf(0426, 8765432, SELECT rif_clie FROM Cliente ORDER BY RANDOM() LIMIT 1, NULL, NULL);
-CALL add_tlf(0412, 3456789, NULL, NULL, SELECT rif_miem FROM Miembro ORDER BY RANDOM() LIMIT 1);
-CALL add_tlf(0414, 4567890, SELECT rif_clie FROM Cliente ORDER BY RANDOM() LIMIT 1, NULL, NULL);
-CALL add_tlf(0424, 5678901, NULL, SELECT cod_pers FROM Contacto ORDER BY RANDOM() LIMIT 1, NULL);
-CALL add_tlf(0416, 6789012, NULL, NULL, SELECT rif_miem FROM Miembro ORDER BY RANDOM() LIMIT 1);
-CALL add_tlf(0426, 7890123, SELECT rif_clie FROM Cliente ORDER BY RANDOM() LIMIT 1, NULL, NULL);
+-- Obtener Cliente
+CREATE OR REPLACE FUNCTION obtener_cliente ()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN SELECT rif_clie FROM Cliente ORDER BY RANDOM() LIMIT 1;
+END
+$$
+
+-- Obtener Miembro
+CREATE OR REPLACE FUNCTION obtener_miembro ()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN SELECT rif_miem FROM Miembro ORDER BY RANDOM() LIMIT 1;
+END
+$$
+
+-- Obtener Persona de Contacto
+CREATE OR REPLACE FUNCTION obtener_contacto ()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN SELECT cod_pers FROM Contacto ORDER BY RANDOM() LIMIT 1
+END
+$$
+
+DO $$
+DECLARE
+    clie varchar(20);
+    pers integer;
+    miem varchar(20);
+BEGIN
+    clie := obtener_cliente();
+    pers := obtener_contacto();
+    miem := obtener_miembro();
+    CALL add_tlf(0412, 9876543, NULL, pers, NULL);
+    CALL add_tlf(0414, 1234567, clie, NULL, NULL);
+    CALL add_tlf(0424, 7654321, NULL, NULL, miem);
+    clie := obtener_cliente();
+    pers := obtener_contacto();
+    miem := obtener_miembro();
+    CALL add_tlf(0416, 2345678, NULL, pers , NULL);
+    CALL add_tlf(0426, 8765432, clie, NULL, NULL);
+    CALL add_tlf(0412, 3456789, NULL, NULL, miem);
+    clie := obtener_cliente();
+    pers := obtener_contacto();
+    miem := obtener_miembro();
+    CALL add_tlf(0414, 4567890, clie, NULL, NULL);
+    CALL add_tlf(0424, 5678901, NULL, pers, NULL);
+    CALL add_tlf(0416, 6789012, NULL, NULL, miem);
+    clie := obtener_cliente();
+    CALL add_tlf(0426, 7890123, clie, NULL, NULL);
+END
+$$;
