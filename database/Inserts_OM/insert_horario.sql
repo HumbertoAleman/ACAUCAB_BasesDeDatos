2a36ff878ddef140a3a73792d450f5b2ed7c3524
Eiderwoss <oscarmanrique.sta@gmail.com>
2025-06-14 20:26:49 -0400

    fix: Changed all á,é,í,ó,ú to their normal form in insert_horario.sql

--- database/Inserts_OM/insert_horario.sql
+++ database/Inserts_OM/insert_horario.sql
@@ -9,11 +9,11 @@ $$
 
 CALL add_horario('08:00', '16:00', 'Lunes');
 CALL add_horario('09:00', '17:00', 'Martes');
-CALL add_horario('10:00', '18:00', 'Miércoles');
+CALL add_horario('10:00', '18:00', 'Miercoles');
 CALL add_horario('07:30', '15:30', 'Jueves');
 CALL add_horario('12:00', '20:00', 'Viernes');
-CALL add_horario('08:00', '14:00', 'Sábado');
+CALL add_horario('08:00', '14:00', 'Sabado');
 CALL add_horario('09:00', '13:00', 'Domingo');
 CALL add_horario('14:00', '22:00', 'Lunes');
 CALL add_horario('16:00', '00:00', 'Viernes');
-CALL add_horario('18:00', '02:00', 'Sábado');
+CALL add_horario('18:00', '02:00', 'Sabado');
