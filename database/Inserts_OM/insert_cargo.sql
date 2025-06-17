88a544872d36449853e4f780f60be42017bb5590
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-17 07:51:19 -0400

    fix: Fixed PROCEDURE and formatted

--- database/Inserts_OM/insert_cargo.sql
+++ database/Inserts_OM/insert_cargo.sql
@@ -1,20 +1,30 @@
-CREATE OR REPLACE PROCEDURE add_cargo(nombre varchar(40))
-LANGUAGE plpgsql
-AS $$
+CREATE OR REPLACE PROCEDURE add_cargo (nombre varchar(40))
+    AS $$
 BEGIN
     INSERT INTO Cargo (nombre_carg)
-    VALUES (nombre);
-END;
+        VALUES (nombre);
+END
 $$
+LANGUAGE plpgsql;
 
-CALL add_cargo('Administrador');
-CALL add_cargo('Gerente');
-CALL add_cargo('Analista');
-CALL add_cargo('Jefe');
-CALL add_cargo('Especialista de Marketing');
-CALL add_cargo('Contador');
-CALL add_cargo('Coordinador de Eventos');
-CALL add_cargo('Gestor de Inventario');
-CALL add_cargo('Especialista de Reportes');
-CALL add_cargo('Empleado');
-CALL add_cargo('Vigilante');
+CALL add_cargo ('Administrador');
+
+CALL add_cargo ('Gerente');
+
+CALL add_cargo ('Analista');
+
+CALL add_cargo ('Jefe');
+
+CALL add_cargo ('Especialista de Marketing');
+
+CALL add_cargo ('Contador');
+
+CALL add_cargo ('Coordinador de Eventos');
+
+CALL add_cargo ('Gestor de Inventario');
+
+CALL add_cargo ('Especialista de Reportes');
+
+CALL add_cargo ('Empleado');
+
+CALL add_cargo ('Vigilante');
