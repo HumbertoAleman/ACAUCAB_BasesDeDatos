5c361015dcd9e2af94c8994f2633cb64d119806c
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-17 07:52:16 -0400

    fix: Fixed PROCEDURE in horario.sql

--- database/Inserts_OM/insert_horario.sql
+++ database/Inserts_OM/insert_horario.sql
@@ -1,19 +1,28 @@
 CREATE OR REPLACE PROCEDURE add_horario (hora_ini time, hora_fin time, dia varchar(20))
-LANGUAGE plpgsql
-AS $$
+    AS $$
 BEGIN
     INSERT INTO Horario (hora_ini_hora, hora_fin_hora, dia_hora)
-    VALUES (hora_ini, hora_fin, dia);
-END;
+        VALUES (hora_ini, hora_fin, dia);
+END
 $$
+LANGUAGE plpgsql;
 
-CALL add_horario('08:00', '16:00', 'Lunes');
-CALL add_horario('09:00', '17:00', 'Martes');
-CALL add_horario('10:00', '18:00', 'Miercoles');
-CALL add_horario('07:30', '15:30', 'Jueves');
-CALL add_horario('12:00', '20:00', 'Viernes');
-CALL add_horario('08:00', '14:00', 'Sabado');
-CALL add_horario('09:00', '13:00', 'Domingo');
-CALL add_horario('14:00', '22:00', 'Lunes');
-CALL add_horario('16:00', '00:00', 'Viernes');
-CALL add_horario('18:00', '02:00', 'Sabado');
+CALL add_horario ('08:00', '16:00', 'Lunes');
+
+CALL add_horario ('09:00', '17:00', 'Martes');
+
+CALL add_horario ('10:00', '18:00', 'Miercoles');
+
+CALL add_horario ('07:30', '15:30', 'Jueves');
+
+CALL add_horario ('12:00', '20:00', 'Viernes');
+
+CALL add_horario ('08:00', '14:00', 'Sabado');
+
+CALL add_horario ('09:00', '13:00', 'Domingo');
+
+CALL add_horario ('14:00', '22:00', 'Lunes');
+
+CALL add_horario ('16:00', '00:00', 'Viernes');
+
+CALL add_horario ('18:00', '02:00', 'Sabado');
