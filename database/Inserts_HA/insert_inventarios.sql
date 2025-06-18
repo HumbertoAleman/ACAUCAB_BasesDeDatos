63969a150e12204528c72a96e6e1ce7c5abb6376
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-18 17:32:59 -0400

    chore + fix: Removed unnecessary DELETE + tweaked amounts in INSERTs

--- database/Inserts_HA/insert_inventarios.sql
+++ database/Inserts_HA/insert_inventarios.sql
@@ -17,7 +17,7 @@ BEGIN
                     CERV_PRES)
                 LOOP
                     INSERT INTO Inventario_Evento (fk_cerv_pres_1, fk_cerv_pres_2, fk_even, cant_pres, precio_actual_pres)
-                        VALUES (c_v.fk_cerv, c_v.fk_pres, x, 100, 9.99);
+                        VALUES (c_v.fk_cerv, c_v.fk_pres, x, 500, 9.99);
                 END LOOP;
         END LOOP;
 END;
@@ -31,32 +31,16 @@ DECLARE
     c_v CERV_PRES%ROWTYPE;
     l_t integer;
 BEGIN
-    FOR c_v IN (
-        SELECT
-            *
-        FROM
-            CERV_PRES
-        ORDER BY
-            RANDOM()
-        LIMIT 5)
-    LOOP
-        FOR l_t IN (
-            SELECT
-                cod_luga_tien
-            FROM
-                Lugar_Tienda)
-            LOOP
-                INSERT INTO Inventario_Tienda (fk_cerv_pres_1, fk_cerv_pres_2, fk_tien, cant_pres, precio_actual_pres, fk_luga_tien)
-                    VALUES (c_v.fk_cerv, c_v.fk_pres, 1, 100, 9.99, l_t);
-            END LOOP;
-    END LOOP;
+    FOR c_v IN ( SELECT * FROM CERV_PRES) LOOP
+            FOR l_t IN ( SELECT cod_luga_tien FROM Lugar_Tienda ORDER BY RANDOM() LIMIT 5 )
+                LOOP
+                    INSERT INTO Inventario_Tienda (fk_cerv_pres_1, fk_cerv_pres_2, fk_tien, cant_pres, precio_actual_pres, fk_luga_tien)
+                        VALUES (c_v.fk_cerv, c_v.fk_pres, 1, 100000, 9.99, l_t);
+                END LOOP;
+        END LOOP;
 END;
 $$
 LANGUAGE plpgsql;
-
-DELETE FROM Inventario_Evento;
-
-DELETE FROM Inventario_Tienda;
 
 CALL populate_events_inventories ();
 
