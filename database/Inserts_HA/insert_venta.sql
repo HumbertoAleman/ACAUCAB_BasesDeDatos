2cf5e1576699b0fc3948126ea2e7ea9f738d08dd
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-18 17:38:18 -0400

    chore + fix: Finished cleaning insert_venta.sql

--- database/Inserts_HA/insert_venta.sql
+++ database/Inserts_HA/insert_venta.sql
@@ -1,14 +1,19 @@
-CREATE OR REPLACE FUNCTION get_estatus (nombre varchar (40))
-LANGUAGE plpgsql
+CREATE OR REPLACE FUNCTION get_estatus (nombre varchar(40))
     RETURNS integer
     AS $$
 DECLARE
     result integer;
 BEGIN
-    SELECT cod_esta INTO result FROM Estatus WHERE nombre_esta = nombre;
+    SELECT
+        cod_esta INTO result
+    FROM
+        Estatus
+    WHERE
+        nombre_esta = nombre;
     RETURN result;
-END;
+END
 $$
+LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION create_punc_canj_historial_on_new_venta ()
     RETURNS TRIGGER
@@ -24,15 +29,15 @@ $$
 LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION create_new_estatus_for_venta ()
-RETURNS TRIGGER
-    LANGUAGE plpgsql
+    RETURNS TRIGGER
     AS $$
 BEGIN
     INSERT INTO ESTA_VENT (fk_esta, fk_vent, fecha_ini, fecha_fin)
-    VALUES ( get_estatus('Procesando'), NEW.cod_vent, NEW.fecha_vent, NULL);
+        VALUES (get_estatus ('Pagado'), NEW.cod_vent, NEW.fecha_vent, NULL);
     RETURN NEW;
-END;
+END
 $$
+LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION get_most_recent_punt_clie_punt (clie varchar(20))
     RETURNS integer
@@ -234,6 +239,26 @@ CREATE OR REPLACE TRIGGER set_estatus_for_new_venta
     FOR EACH ROW
     EXECUTE FUNCTION create_new_estatus_for_venta ();
 
+CREATE OR REPLACE FUNCTION get_precio_from_inventario (integer, integer, integer, integer)
+    RETURNS numeric
+    AS $$
+DECLARE
+    res numeric;
+BEGIN
+    SELECT
+        precio_actual_pres
+    FROM
+        Inventario_Tienda
+    WHERE
+        fk_cerv_pres_1 = $1
+        AND fk_cerv_pres_2 = $2
+        AND fk_tien = $3
+        AND fk_luga_tien = $4 INTO res;
+    RETURN res;
+END
+$$
+LANGUAGE plpgsql;
+
 -- Crear un trigger cuando se cree la venta, en el trigger se revisa si es online u offline,
 -- solo se crea PUNT_CLIE cuando la venta es offline
 -- Crear otro trigger cuando se cree un pago utilizando el metodo de pago Punto_Canjeo,
@@ -250,7 +275,6 @@ m_p_fk_meto_pago integer[], -- Array fk_meto_pago
 m_p_monto_pago numeric(8, 2)[], -- Array montos
 -- Parameters for Detalle_Venta
 d_v_cant_deta_vent integer[], -- Array of quantities sold
-d_v_precio_unitario_vent numeric(8, 2)[], -- Array of unit prices
 d_v_fk_inve_tien_1 integer[], -- Array fk_cerv
 d_v_fk_inve_tien_2 integer[], -- Array fk_pres
 d_v_fk_inve_tien_4 integer[] -- fk_luga_tien
@@ -260,25 +284,38 @@ DECLARE
     total_acum integer;
     v_cod_tasa integer;
     v_cod_vent integer;
+    precio_unitario numeric;
     -- Hold generated cod_vent
     i integer;
     -- Loop index
 BEGIN
+    ----
+    -- Create Venta record
+    -- * This shoots set_estatus_for_new_venta that creates ESTA_VENT, in "Pagado" since we assume that it's already paid for
+    ----
     INSERT INTO Venta (fecha_vent, base_imponible_vent, iva_vent, total_vent, online, fk_clie, fk_tien)
         VALUES (p_fecha_vent, 0, 0, 0, p_online, p_fk_clie, p_fk_tien)
     RETURNING
         cod_vent INTO v_cod_vent;
-    --
+    ----
     -- Loop through the item details and insert each item into Detalle_Venta
+    -- * This shoots after_insert_detalle_venta that updates Venta with the price of the item * quantity
+    -- * This shoots remove_from_inventory_after_detalle that updates
+    --     Inventario_Tienda removing the amount bought
+    -- * This shoots add_points_on_venta, in which if the Venta is offline, it adds points to the Client
+    ----
     FOR i IN 1..array_length(d_v_cant_deta_vent, 1)
     LOOP
         total_acum := total_acum + d_v_cant_deta_vent[i];
-        -- Remember that the total of venta is updated with these inserts
+        precio_unitario := get_precio_from_inventario (d_v_fk_inve_tien_1[i], d_v_fk_inve_tien_2[i], p_fk_tien, d_v_fk_inve_tien_4[i]);
         INSERT INTO Detalle_Venta (cant_deta_vent, precio_unitario_vent, fk_vent, fk_inve_tien_1, fk_inve_tien_2, fk_inve_tien_3, fk_inve_tien_4)
-            VALUES (d_v_cant_deta_vent[i], d_v_precio_unitario_vent[i], v_cod_vent, d_v_fk_inve_tien_1[i], d_v_fk_inve_tien_2[i], p_fk_tien, d_v_fk_inve_tien_4[i]);
+            VALUES (d_v_cant_deta_vent[i], precio_unitario, v_cod_vent, d_v_fk_inve_tien_1[i], d_v_fk_inve_tien_2[i], p_fk_tien, d_v_fk_inve_tien_4[i]);
     END LOOP;
-    --
+    ----
     -- Loop through the payment arrays and insert each payment
+    -- * This shoots substract_points_on_venta where if Pago is Punto_Canjeo
+    --     it discounts the apropriate points from the Client
+    ----
     FOR i IN 1..array_length(m_p_fk_meto_pago, 1)
     LOOP
         INSERT INTO Pago (fk_vent, fk_meto_pago, monto_pago, fecha_pago, fk_tasa)
@@ -301,13 +338,12 @@ DECLARE
     v_m_p_fk_meto_pago integer[];
     v_m_p_monto_pago numeric(8, 2)[];
     v_d_v_cant_deta_vent integer[];
-    v_d_v_precio_unitario_vent numeric(8, 2)[];
     v_d_v_fk_inve_tien_1 integer[];
     v_d_v_fk_inve_tien_2 integer[];
     v_d_v_fk_inve_tien_4 integer[];
     entry RECORD;
 BEGIN
-    FOR i IN 1..100 LOOP
+    FOR i IN 1..10 LOOP
         v_fecha_vent := CURRENT_DATE - (RANDOM() * 30)::int;
         v_base_imponible_vent := ROUND(RANDOM() * 1000);
         v_online := (RANDOM() < 0.25);
@@ -342,54 +378,106 @@ BEGIN
         v_d_v_fk_inve_tien_1 := '{}';
         v_d_v_fk_inve_tien_2 := '{}';
         v_d_v_fk_inve_tien_4 := '{}';
-        v_d_v_precio_unitario_vent := '{}';
         v_d_v_fk_inve_tien_1 := array_append(v_d_v_fk_inve_tien_1, entry.fk_cerv_pres_1);
         v_d_v_fk_inve_tien_2 := array_append(v_d_v_fk_inve_tien_2, entry.fk_cerv_pres_2);
         v_d_v_fk_inve_tien_4 := array_append(v_d_v_fk_inve_tien_4, entry.fk_luga_tien);
         v_d_v_cant_deta_vent := ARRAY[RANDOM() * 10::int + 1]::int[];
-        v_d_v_precio_unitario_vent := array_append(v_d_v_precio_unitario_vent, entry.precio_actual_pres);
-        v_m_p_monto_pago := ARRAY[v_d_v_cant_deta_vent[1] * v_d_v_precio_unitario_vent[1]]::int[];
+        v_m_p_monto_pago := ARRAY[v_d_v_cant_deta_vent[1] * get_precio_from_inventario (entry.fk_cerv_pres_1, entry.fk_cerv_pres_2, v_fk_tien, entry.fk_luga_tien)]::int[];
         -- Call the venta_en_tienda procedure with the random values
-        CALL venta_en_tienda (v_fecha_vent, v_online, v_fk_clie, v_fk_tien, v_m_p_fk_meto_pago, v_m_p_monto_pago, v_d_v_cant_deta_vent, v_d_v_precio_unitario_vent, v_d_v_fk_inve_tien_1, v_d_v_fk_inve_tien_2, v_d_v_fk_inve_tien_4);
+        CALL venta_en_tienda (v_fecha_vent, v_online, v_fk_clie, v_fk_tien, v_m_p_fk_meto_pago, v_m_p_monto_pago, v_d_v_cant_deta_vent, v_d_v_fk_inve_tien_1, v_d_v_fk_inve_tien_2, v_d_v_fk_inve_tien_4);
     END LOOP;
 END;
 $$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION get_random_juridico ()
+    RETURNS varchar (
+        20
+)
+        AS $$
+DECLARE
+    res varchar(20);
+BEGIN
+    SELECT
+        rif_clie
+    FROM
+        cliente
+    WHERE
+        tipo_clie = 'Juridico' INTO res;
+    RETURN res;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_cheque_random ()
+    RETURNS integer
+    AS $$
+DECLARE
+    res integer;
+BEGIN
+    SELECT
+        fk_meto_pago
+    FROM
+        cheque INTO res;
+    RETURN res;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE create_orden_compra_10_productos ()
+    AS $$
+DECLARE
+    x integer;
+    i integer;
+    res record;
+    cant_item int[];
+    precio_total numeric = 0;
+    fk_inve_tien_1 int[];
+    fk_inve_tien_2 int[];
+    fk_inve_tien_4 int[];
+BEGIN
+    FOR i IN 1..10 LOOP
+        -- Set Initial Values
+        cant_item := '{}';
+        precio_total := 0;
+        fk_inve_tien_1 := '{}';
+        fk_inve_tien_2 := '{}';
+        fk_inve_tien_4 := '{}';
+        FOR x IN 1..10 LOOP
+            SELECT
+                fk_cerv_pres_1,
+                fk_cerv_pres_2,
+                precio_actual_pres,
+                fk_luga_tien INTO res
+            FROM
+                Inventario_Tienda
+            WHERE
+                fk_tien = 1 AND
+				fk_cerv_pres_1 <> ALL(fk_inve_tien_1)
+            ORDER BY
+                RANDOM();
+            cant_item := array_append(cant_item, ROUND(RANDOM() * 200) + 200);
+            precio_total := precio_total + (cant_item[x] * res.precio_actual_pres);
+            fk_inve_tien_1 := array_append(fk_inve_tien_1, res.fk_cerv_pres_1);
+            fk_inve_tien_2 := array_append(fk_inve_tien_2, res.fk_cerv_pres_2);
+            fk_inve_tien_4 := array_append(fk_inve_tien_4, res.fk_luga_tien);
+        END LOOP;
+        precio_total := precio_total * 1.16;
+        ----
+        -- Make them TODAY
+        -- Make them ONLINE
+        -- Make them from JURIDICO
+        -- Make them from SHOP 1
+        -- Make them 1 PAYMENT, CHEQUE
+        -- Make them 1 PAYMENT, CALCULATED HERE
+        -- Arrays of Bought Items, Qty, Beer, Size, ShopPlace
+        CALL venta_en_tienda (CURRENT_DATE, TRUE, get_random_juridico (), 1, ARRAY[get_cheque_random ()], ARRAY[precio_total], cant_item, fk_inve_tien_1, fk_inve_tien_2, fk_inve_tien_4);
+        ----
+    END LOOP;
+END
+$$
+LANGUAGE plpgsql;
+
+CALL create_orden_compra_10_productos ();
+
 CALL realizar_ventas_aleatorias ();
-
-SELECT
-    *
-FROM
-    punt_clie
-WHERE
-    fk_clie = 'J314865203';
-
-SELECT
-    V.cod_vent AS "Venta",
-    V.online AS "Online",
-    D_V.cant_deta_vent AS "Cantidad",
-    D_V.precio_unitario_vent AS "Precio U",
-    V.iva_vent AS "IVA",
-    V.base_imponible_vent AS "Imponible",
-    V.total_vent AS "Total",
-    C.rif_clie AS "Cliente",
-    Ce.nombre_cerv AS "Cerveza",
-    T_C.nombre_tipo_cerv AS "Tipo",
-    P.nombre_pres AS "Presentacion",
-    T.nombre_tien AS "Tienda",
-    L_T.nombre_luga_tien AS "Lugar en Tienda",
-    P_C.cant_puntos_canj AS "Puntos Delta",
-    P_C.cant_puntos_acum AS "Puntos Despues"
-FROM
-    Venta AS V
-    FULL JOIN Detalle_Venta AS D_V ON D_V.fk_vent = V.cod_vent
-    FULL JOIN Cliente AS C ON V.fk_clie = C.rif_clie
-    FULL JOIN Cerveza AS Ce ON D_V.fk_inve_tien_1 = Ce.cod_cerv
-    FULL JOIN Tipo_Cerveza AS T_C ON Ce.fk_tipo_cerv = T_C.cod_tipo_cerv
-    FULL JOIN Presentacion AS P ON D_V.fk_inve_tien_2 = P.cod_pres
-    FULL JOIN Tienda AS T ON D_V.fk_inve_tien_3 = T.cod_tien
-    FULL JOIN Lugar_Tienda AS L_T ON D_V.fk_inve_tien_4 = L_T.cod_luga_tien
-    FULL JOIN PUNT_CLIE AS P_C ON V.cod_vent = P_C.fk_vent
-WHERE
-    V.cod_vent = 1332;
