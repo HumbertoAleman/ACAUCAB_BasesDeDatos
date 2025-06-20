92b885710dbe71a96a0366afb30345cd0a81a654
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-20 19:24:50 -0400

    fix: Removed unnecessary PROCEDURE from estatus.sql

--- database/Inserts_OM/insert_estatus.sql
+++ database/Inserts_OM/insert_estatus.sql
@@ -1,33 +1,16 @@
-CREATE OR REPLACE PROCEDURE add_estatus (nombre varchar(20), descripcion text)
-    AS $$
-BEGIN
-    INSERT INTO Estatus (nombre_esta, descripcion_esta)
-        VALUES (nombre, descripcion);
-END
-$$
-LANGUAGE plpgsql;
-
--- Estatus de Compras
-CALL add_estatus ('Por aprobar', 'El jefe del departamento de compras aún no ha aceptado la solicitud de reposición');
-
-CALL add_estatus ('Por pagar', 'El jefe del departamento de compras aun no ha realizado el pago de compra a proveedor');
-
-CALL add_estatus ('Cancelado', 'El jefe de del departamento de compras ha cancelado la orden de reposición');
-
-CALL add_estatus ('Compra Pagada', 'El jefe de del departamento de compras ha cancelado la orden de reposición');
-
--- Estatus de Ventas
-CALL add_estatus ('Pagado', 'La entrega u presupuesto ha sido pagada');
-
-CALL add_estatus ('En Camino', 'La entrega esta en camino a su destino');
-
-CALL add_estatus ('Entregado', 'La venta ha concluido');
-
--- Estatus de Eventos
-CALL add_estatus ('Pendiente', 'El evento ha sido formalizado pero aún no ha iniciado');
-
-CALL add_estatus ('En curso', 'El evento esta ocurriendo en estos instantes');
-
-CALL add_estatus ('Cancelado', 'El evento ha sido cancelado');
-
-CALL add_estatus ('Finalizado', 'El evento ha finalizado');
+INSERT INTO Estatus (nombre_esta, descripcion_esta)
+    VALUES
+        -- Estatus de Compras
+        ('Por aprobar', 'El jefe del departamento de compras aún no ha aceptado la solicitud de reposición'),
+        ('Por pagar', 'El jefe del departamento de compras aun no ha realizado el pago de compra a proveedor'),
+        ('Cancelado', 'El jefe de del departamento de compras ha cancelado la orden de reposición'),
+        ('Compra Pagada', 'El jefe de del departamento de compras ha cancelado la orden de reposición'),
+        -- Estatus de Ventas
+        ('Pagado', 'La entrega u presupuesto ha sido pagada'),
+        ('En Camino', 'La entrega esta en camino a su destino'),
+        ('Entregado', 'La venta ha concluido'),
+        -- Estatus de Eventos
+        ('Pendiente', 'El evento ha sido formalizado pero aún no ha iniciado'),
+        ('En curso', 'El evento esta ocurriendo en estos instantes'),
+        ('Cancelado', 'El evento ha sido cancelado'),
+        ('Finalizado', 'El evento ha finalizado');
