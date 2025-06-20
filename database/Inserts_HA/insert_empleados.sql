2dfd3217de49d803c192b9aa3d13f3b9f10a86c4
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-20 19:31:42 -0400

    fix: Fixed empleados formatting

--- database/Inserts_HA/insert_empleados.sql
+++ database/Inserts_HA/insert_empleados.sql
@@ -13,22 +13,22 @@ BEGIN
 END;
 $$;
 
-CALL add_empleado(31279920, 'Serita', 'Jewel', 'Herrick', 'Blevins', 73.02);
+CALL add_empleado (31279920, 'Serita', 'Jewel', 'Herrick', 'Blevins', 73.02);
 
-CALL add_empleado(48422996, 'Thora', 'Candra', 'Ferrer', 'Pina', 46.97);
+CALL add_empleado (48422996, 'Thora', 'Candra', 'Ferrer', 'Pina', 46.97);
 
-CALL add_empleado(2428796, 'Barney', 'Merlin', 'Atchison', 'Hooker', 18.39);
+CALL add_empleado (2428796, 'Barney', 'Merlin', 'Atchison', 'Hooker', 18.39);
 
-CALL add_empleado(26806759, 'Stacey', 'Eura', 'Seeley', 'Thomsen', 85.59);
+CALL add_empleado (26806759, 'Stacey', 'Eura', 'Seeley', 'Thomsen', 85.59);
 
-CALL add_empleado(4221028, 'Kati', 'Serafina', 'Valencia', 'Guillory', 53.06);
+CALL add_empleado (4221028, 'Kati', 'Serafina', 'Valencia', 'Guillory', 53.06);
 
-CALL add_empleado(1418738, 'Loni', 'Hershel', 'Stubblefield', 'Ahmad-Alley', 20.58);
+CALL add_empleado (1418738, 'Loni', 'Hershel', 'Stubblefield', 'Ahmad-Alley', 20.58);
 
-CALL add_empleado(518945, 'Loree', 'Alpha', 'Sparks', 'Sell', 62.83);
+CALL add_empleado (518945, 'Loree', 'Alpha', 'Sparks', 'Sell', 62.83);
 
-CALL add_empleado(46565698, 'Vashti', 'Conchita', 'Sadler', 'Carpenter', 67.6);
+CALL add_empleado (46565698, 'Vashti', 'Conchita', 'Sadler', 'Carpenter', 67.6);
 
-CALL add_empleado(42233317, 'Bula', 'Norbert', 'Amaral', 'Wooley', 55.88);
+CALL add_empleado (42233317, 'Bula', 'Norbert', 'Amaral', 'Wooley', 55.88);
 
-CALL add_empleado(40959596, 'Jewel', 'Elene', 'Bray', 'Penn', 18.01);
+CALL add_empleado (40959596, 'Jewel', 'Elene', 'Bray', 'Penn', 18.01);
