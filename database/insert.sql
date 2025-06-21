3b8f74822db82e5744161dead04e5296c7e5c4fe
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-20 21:47:35 -0400

    feat: Added Estatus to insert.sql

--- database/insert.sql
+++ database/insert.sql
@@ -608,6 +608,24 @@ INSERT INTO Beneficio (nombre_bene, cantidad_bene)
 	('Días de Enfermedad Pagados', 2000),
 	('Beneficios de Bienestar', 1000);
 
+-- [[ INSERT ESTATUS, INDEPENDENT ]]--
+INSERT INTO Estatus (nombre_esta, descripcion_esta)
+    VALUES
+        -- Estatus de Compras
+        ('Por aprobar', 'El jefe del departamento de compras aún no ha aceptado la solicitud de reposición'),
+        ('Por pagar', 'El jefe del departamento de compras aun no ha realizado el pago de compra a proveedor'),
+        ('Cancelado', 'El jefe de del departamento de compras ha cancelado la orden de reposición'),
+        ('Compra Pagada', 'El jefe de del departamento de compras ha cancelado la orden de reposición'),
+        -- Estatus de Ventas
+        ('Pagado', 'La entrega u presupuesto ha sido pagada'),
+        ('En Camino', 'La entrega esta en camino a su destino'),
+        ('Entregado', 'La venta ha concluido'),
+        -- Estatus de Eventos
+        ('Pendiente', 'El evento ha sido formalizado pero aún no ha iniciado'),
+        ('En curso', 'El evento esta ocurriendo en estos instantes'),
+        ('Cancelado', 'El evento ha sido cancelado'),
+        ('Finalizado', 'El evento ha finalizado');
+
 --[[ INSERT ESTADOS, 
 -- DEPENDENT on PROCEDURE ESTADOS ]]--
 CALL insert_estados(ARRAY ['Amazonas', 'Anzoategui', 'Apure', 'Aragua', 'Barinas', 'Bolivar', 'Carabobo', 'Cojedes', 'Delta Amacuro', 'Distrito Capital', 'Falcon', 'Guarico', 'La Guaira', 'Lara', 'Merida', 'Miranda', 'Monagas', 'Nueva Esparta', 'Portuguesa', 'Sucre', 'Tachira', 'Trujillo', 'Yaracuy', 'Zulia']);
