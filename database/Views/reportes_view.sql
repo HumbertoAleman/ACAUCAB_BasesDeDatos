1e2cccadc1f571d0e7021b2c3aee8cb8f6228d6f
Eiderwoss <oscarmanrique.sta@gmail.com>
2025-06-21 18:27:46 -0400

    fix: Fixed some Alias in reportes_view.sql

--- database/Views/reportes_view.sql
+++ database/Views/reportes_view.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE VIEW tipo_cliente_view AS
-    SELECT tipo_clie 'Tipo de Cliente', fecha_ingr_clie 'Fecha de ingreso'
+    SELECT tipo_clie "Tipo de Cliente", fecha_ingr_clie "Fecha de ingreso"
     FROM Cliente
     Order By fecha_ingr_clie ASC;
 
 CREATE OR REPLACE VIEW horas_trabajo_view AS
-    SELECT e.cod_empl 'Numero Empleado', e.ci_empl 'Cedula'
+    SELECT e.cod_empl "Numero Empleado", e.ci_empl "Cedula", a.fecha_hora_ini_asis "Entrada", a.fecha_hora_fin_asis "Salida"
     FROM Asistencia a, EMPL_CARG ec, Empleado e
     WHERE a.fk_empl_carg_1 = ec.fk_empl AND a.fk_empl_carg_2 = ec.fk_carg AND a.fk_empl_carg_3 = ec.cod_empl_carg
-        AND ec.fk_empl = e.cod_empl;
+    AND ec.fk_empl = e.cod_empl;
