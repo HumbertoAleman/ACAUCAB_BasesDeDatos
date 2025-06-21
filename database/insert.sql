32a8efa77e13cfb67d6662341fb3f08ba8f6c207
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-20 21:26:02 -0400

    feat: Added EMPL_CARG to insert.sql

--- database/insert.sql
+++ database/insert.sql
@@ -1,5 +1,41 @@
 --[[[ FUNCTION BEGIN ]]]--
 
+-- Buscar Empleado
+CREATE OR REPLACE FUNCTION get_empleado (ci integer)
+    RETURNS integer
+    AS $$
+DECLARE
+    res integer;
+BEGIN
+    SELECT
+        cod_empl
+    FROM
+        Empleado
+    WHERE
+        ci_empl = ci INTO res;
+    RETURN res;
+END
+$$
+LANGUAGE plpgsql;
+
+-- Buscar Cargo
+CREATE OR REPLACE FUNCTION get_cargo (nombre varchar(40))
+    RETURNS integer
+    AS $$
+DECLARE
+    res integer;
+BEGIN
+    SELECT
+        cod_carg
+    FROM
+        Cargo
+    WHERE
+        nombre_carg = nombre INTO res;
+    RETURN res;
+END
+$$
+LANGUAGE plpgsql;
+
 -- Get parroquia from name
 CREATE OR REPLACE FUNCTION get_parroquia (nom_parroquia varchar(40))
     RETURNS integer
@@ -34,6 +70,25 @@ BEGIN
     ORDER BY
         cod_tien
     LIMIT 1 INTO res;
+    RETURN res;
+END
+$$
+LANGUAGE plpgsql;
+
+-- Buscar Departamento In Tienda 1
+CREATE OR REPLACE FUNCTION get_departamento_1 (nombre varchar(40))
+    RETURNS integer
+    AS $$
+DECLARE
+    res integer;
+BEGIN
+    SELECT
+        cod_depa
+    FROM
+        Departamento
+    WHERE
+        fk_tien = get_tienda_1 ()
+        AND nombre_depa = nombre INTO res;
     RETURN res;
 END
 $$
@@ -153,6 +208,20 @@ BEGIN
         VALUES ('admin', p_nom || ' ' || p_ape, 300, x);
 END;
 $$;
+
+-- [[ PROCEDURE EMPL_CARG
+-- DEPENDENT get_tienda_1, get_departamento_1 ]]--
+
+CREATE OR REPLACE PROCEDURE add_empl_carg_to_tien_1 (ci integer, nombre_carg varchar(40), ini date, fin date, montoTotal integer, nombre_derp varchar(40))
+    AS $$
+DECLARE
+    fk_depa1 integer;
+BEGIN
+    INSERT INTO EMPL_CARG (fk_empl, fk_carg, fecha_ini, fecha_fin, cantidad_total_salario, fk_depa_1, fk_depa_2)
+        VALUES (get_empleado (ci), get_cargo (nombre_carg), ini, fin, montoTotal, get_departamento (nombre_derp), get_tienda_1 ());
+END
+$$
+LANGUAGE plpgsql;
 
 --[[[ INSERT BEGIN ]]]--
 
@@ -728,3 +797,26 @@ CALL add_empleado (46565698, 'Vashti', 'Conchita', 'Sadler', 'Carpenter', 67.6);
 CALL add_empleado (42233317, 'Bula', 'Norbert', 'Amaral', 'Wooley', 55.88);
 
 CALL add_empleado (40959596, 'Jewel', 'Elene', 'Bray', 'Penn', 18.01);
+
+-- [[ INSERT EMPL_CARG ]]--
+
+CALL add_empl_carg_to_tien_1 (40959596, 'Empleado', '2024-10-10', NULL, 25000, 'Compras');
+
+CALL add_empl_carg_to_tien_1 (48422996, 'Empleado', '2024-10-10', NULL, 25000, 'Ventas');
+
+CALL add_empl_carg_to_tien_1 (2428796, 'Empleado', '2024-10-10', NULL, 25000, 'Despacho');
+
+CALL add_empl_carg_to_tien_1 (26806759, 'Empleado', '2024-10-10', NULL, 25000, 'Entrega');
+
+CALL add_empl_carg_to_tien_1 (4221028, 'Empleado', '2024-10-10', NULL, 25000, 'Marketing');
+
+CALL add_empl_carg_to_tien_1 (1418738, 'Empleado', '2024-10-10', NULL, 25000, 'Recursos Humanos');
+
+CALL add_empl_carg_to_tien_1 (518945, 'Empleado', '2024-10-10', NULL, 25000, 'Finanzas');
+
+CALL add_empl_carg_to_tien_1 (46565698, 'Empleado', '2024-10-10', NULL, 25000, 'Seguridad');
+
+CALL add_empl_carg_to_tien_1 (42233317, 'Empleado', '2024-10-10', NULL, 25000, 'Limpieza');
+
+CALL add_empl_carg_to_tien_1 (40959596, 'Empleado', '2024-10-10', NULL, 25000, 'Mantenimiento');
+
