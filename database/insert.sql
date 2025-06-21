80f9b75d1a4b2107d6ca52a36f28f3664a3b8c47
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-20 21:56:23 -0400

    feat: Added Privilegio, PRIV_ROL into insert.sql

--- database/insert.sql
+++ database/insert.sql
@@ -503,6 +503,48 @@ END;
 $$
 LANGUAGE plpgsql;
 
+--[[ PROCEDURE PRIVILEGIO ]]--
+CREATE OR REPLACE PROCEDURE privileges ()
+    AS $$
+DECLARE
+    x text;
+    y text;
+    perms varchar(10)[] = ARRAY['create', 'read', 'update', 'delete'];
+BEGIN
+    FOR x IN (
+        SELECT
+            relname
+        FROM
+            pg_stat_user_tables)
+        LOOP
+            FOREACH y IN ARRAY perms LOOP
+                INSERT INTO Privilegio (nombre_priv, descripcion_priv)
+                    VALUES (x || '_' || y, 'Privilegio para ' || y || ' la tabla ' || x);
+            END LOOP;
+        END LOOP;
+END
+$$
+LANGUAGE plpgsql;
+
+--[[ PROCEDURE PRIV_ROL ]]--
+CREATE OR REPLACE PROCEDURE admin_privileges ()
+    AS $$
+DECLARE
+    x integer;
+BEGIN
+    FOR x IN (
+        SELECT
+            cod_priv
+        FROM
+            Privilegio)
+        LOOP
+            INSERT INTO PRIV_ROL (fk_rol, fk_priv)
+                VALUES (500, x);
+        END LOOP;
+END
+$$
+LANGUAGE plpgsql;
+
 --[[[ INSERT BEGIN ]]]--
 
 --[[ INSERT ROL, INDEPENDENT ]]--
@@ -1299,3 +1341,8 @@ CALL insert_contacto_random ('Casey', 'Aldus', 'Semerad', 'Kench');
 CALL insert_contacto_random ('Alina', 'Ezra', 'Canham', 'Cole');
 
 CALL insert_contacto_random ('Oralie', 'Jaimie', 'Longstreeth', 'Eldered');
+
+--[[ INSERT INTO PRIVILEGIO, PRIV_ROL ]]--
+CALL privileges ();
+
+CALL admin_privileges ();
