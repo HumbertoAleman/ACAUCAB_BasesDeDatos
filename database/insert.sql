db474a93207f4d5098f77e1189ab35b1e40fc605
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-20 22:38:47 -0400

    feat: Added Descuento, DESC_CERV to insert.sql

--- database/insert.sql
+++ database/insert.sql
@@ -729,6 +729,36 @@ END;
 $$
 LANGUAGE plpgsql;
 
+--[[ PROCEDURE DESC_CERV ]]--
+CREATE OR REPLACE PROCEDURE give_random_descuentos ()
+    AS $$
+DECLARE
+    c_v record;
+    d record;
+BEGIN
+    FOR c_v IN (
+        SELECT
+            *
+        FROM
+            CERV_PRES
+        ORDER BY
+            RANDOM()
+        LIMIT 100)
+    LOOP
+        SELECT
+            *
+        FROM
+            Descuento
+        ORDER BY
+            RANDOM()
+        LIMIT 1 INTO d;
+        INSERT INTO DESC_CERV (fk_desc, fk_cerv_pres_1, fk_cerv_pres_2, porcentaje_desc)
+            VALUES (d.cod_desc, c_v.fk_cerv, c_v.fk_pres, ROUND(RANDOM() * 70) + 10);
+    END LOOP;
+END
+$$
+LANGUAGE plpgsql;
+
 --[[[ INSERT BEGIN ]]]--
 
 --[[ INSERT ROL, INDEPENDENT ]]--
@@ -884,6 +914,18 @@ INSERT INTO Presentacion (nombre_pres, capacidad_pres)
     ('Pinta Americana', 473),
     ('Pinta Inglesa', 568);
 
+--[[ INSERT DESCUENTO, INDEPENDENT ]]--
+INSERT INTO DESCUENTO (descripcion_desc, fecha_ini_desc, fecha_fin_desc)
+    VALUES ('Descuento de Navidad', '2022-12-01', '2023-01-01'),
+    ('Descuento DiarioUnaCerveza', '2023-01-01', '2023-01-21'),
+    ('Descuento DiarioUnaCerveza', '2023-01-21', '2023-02-10'),
+    ('Descuento DiarioUnaCerveza', '2023-02-10', '2023-03-02'),
+    ('Descuento DiarioUnaCerveza', '2023-03-02', '2023-03-22'),
+    ('Descuento DiarioUnaCerveza', '2023-03-22', '2023-04-11'),
+    ('Descuento DiarioUnaCerveza', '2023-04-11', '2023-05-01'),
+    ('Descuento DiarioUnaCerveza', '2023-05-01', '2023-05-21'),
+    ('Descuento DiarioUnaCerveza', '2023-05-21', '2023-06-10'),
+    ('Descuento DiarioUnaCerveza', '2023-06-10', '2023-06-30');
 
 --[[ INSERT ESTADOS, 
 -- DEPENDENT on PROCEDURE ESTADOS ]]--
@@ -2233,3 +2275,6 @@ CALL relate_cara_cerv ('Aldarra Mantuana', 'Carbonatacion', 'Media');
 
 --[[ INSERT CERV_PRES ]]--
 CALL pres_to_cerv_all ();
+
+--[[ INSERT DESC_CERV ]]--
+CALL give_random_descuentos ();
