01ff06f9aeff10f4cc64945566f2467dab22f78f
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-20 21:52:35 -0400

    feat: Added Contacto to insert.sql

--- database/insert.sql
+++ database/insert.sql
@@ -484,6 +484,25 @@ END;
 $$
 LANGUAGE plpgsql;
 
+-- [[ PROCEDURE CONTACTO ]]--
+CREATE OR REPLACE PROCEDURE insert_contacto_random (text, text, text, text)
+    AS $$
+DECLARE
+    x text;
+BEGIN
+    SELECT
+        rif_miem INTO x
+    FROM
+        Miembro
+    ORDER BY
+        RANDOM()
+    LIMIT 1;
+    INSERT INTO Contacto (primer_nom_pers, segundo_nom_pers, primer_ape_pers, segundo_ape_pers, fk_miem)
+        VALUES ($1, $2, $3, $4, x);
+END;
+$$
+LANGUAGE plpgsql;
+
 --[[[ INSERT BEGIN ]]]--
 
 --[[ INSERT ROL, INDEPENDENT ]]--
@@ -1259,3 +1278,24 @@ CALL add_miembro_random ('J438201756', 'Kernel Energy ', 'Peru B.V', '5593 Holli
 CALL add_miembro_random ('J325497068', 'Upc Industries SIA', 'Ending International S.A', '9767 Raydon Avenue, Provo, Florida, 56944', 'https://www.navigator.bømlo.no/lucy');
 
 CALL add_miembro_random ('J471039258', 'Coordinate Industries Company', 'Emotions International ', '9059 Reddisher Circle, Grand Prairie, Maine, 73188', 'http://condos.com/rock.aspx');
+
+--[[ INSERT CONTACTO ]]--
+CALL insert_contacto_random ('Salmon', 'Ruby', 'Flowerden', 'Aizkovitch');
+
+CALL insert_contacto_random ('Elvina', 'Harlen', 'Crack', 'Hurlestone');
+
+CALL insert_contacto_random ('Hinze', 'Minetta', 'Antonelli', 'Winfrey');
+
+CALL insert_contacto_random ('Sibbie', 'Ettore', 'Cogan', 'Philson');
+
+CALL insert_contacto_random ('Gene', 'Alexandra', 'Snedden', 'Blankman');
+
+CALL insert_contacto_random ('Annmaria', 'Berti', 'Greggs', 'Fawdrie');
+
+CALL insert_contacto_random ('Gertrude', 'Nan', 'Loutheane', 'Drezzer');
+
+CALL insert_contacto_random ('Casey', 'Aldus', 'Semerad', 'Kench');
+
+CALL insert_contacto_random ('Alina', 'Ezra', 'Canham', 'Cole');
+
+CALL insert_contacto_random ('Oralie', 'Jaimie', 'Longstreeth', 'Eldered');
