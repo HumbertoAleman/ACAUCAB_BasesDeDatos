04b9a5938285a810fc7fa43fcf1e578316059c81
Eiderwoss <oscarmanrique.sta@gmail.com>
2025-06-22 01:04:14 -0400

    feat: Added new view: rentabilidad_tipo_view

--- database/Views/reportes_view.sql
+++ database/Views/reportes_view.sql
@@ -10,9 +10,28 @@ CREATE OR REPLACE VIEW horas_trabajo_view AS
     AND ec.fk_empl = e.cod_empl;
 
 CREATE OR REPLACE VIEW productos_generan_repo_view AS
-    SELECT c.cod_cerv, c.nombre_cerv, cp.fk_cerv, cp.fk_pres, dc.fk_cerv_pres_1 "Cerveza DC", dc.fk_cerv_pres_2 "Presentacion DC",
-        it.fk_cerv_pres_1 "Cerveza Inventario", it.fk_cerv_pres_2 "Presentación Inventario", dv.fk_inve_tien_1 "Cerveza DV", dv.fk_inve_tien_2 "Presentacion DV"
-    FROM Detalle_Compra dc, Detalle_Venta dv, CERV_PRES cp, Cerveza c, Inventario_Tienda it
-    WHERE c.cod_cerv = cp.fk_cerv AND cp.fk_cerv = dc.fk_cerv_pres_1 AND cp.fk_pres = dc.fk_cerv_pres_2
-        AND cp.fk_cerv = it.fk_cerv_pres_1 AND cp.fk_pres = it.fk_cerv_pres_2 AND it.fk_cerv_pres_1 = dv.fk_inve_tien_1
-        AND it.fk_cerv_pres_2 = dv.fk_inve_tien_2;
+    SELECT DISTINCT c.cod_cerv "Código de Cerveza", c.nombre_cerv "Nombre", p.cod_pres "Código de Presentación", p.nombre_pres "Nombre Presentación"
+    FROM Detalle_Compra dc, Detalle_Venta dv, CERV_PRES cp, Cerveza c, Inventario_Tienda it, Presentacion p
+    WHERE c.cod_cerv = cp.fk_cerv 
+        AND cp.fk_pres = p.cod_pres
+        AND cp.fk_cerv = dc.fk_cerv_pres_1
+        AND cp.fk_pres = dc.fk_cerv_pres_2
+        AND cp.fk_cerv = it.fk_cerv_pres_1
+        AND cp.fk_pres = it.fk_cerv_pres_2
+        AND it.fk_cerv_pres_1 = dv.fk_inve_tien_1
+        AND it.fk_cerv_pres_2 = dv.fk_inve_tien_2
+        AND dv.fk_inve_tien_1 = dc.fk_cerv_pres_1
+        AND dv.fk_inve_tien_2 = dc.fk_cerv_pres_2;
+
+CREATE OR REPLACE VIEW rentabilidad_tipo_view AS
+	SELECT tc.cod_tipo_cerv "Código Tipo", tc.nombre_tipo_cerv "Nombre Tipo de Cerveza",v.fecha_vent "Fecha de Venta", SUM(dv.precio_unitario_vent * dv.cant_deta_vent) "Ganancia total entre todas las Ventas"
+    FROM Presentacion p, Detalle_Venta dv, Inventario_Tienda it, CERV_PRES cp, Cerveza c, Tipo_Cerveza tc, Venta v
+    WHERE p.cod_pres = cp.fk_pres
+        AND c.cod_cerv = cp.fk_cerv
+        AND c.fk_tipo_cerv = tc.cod_tipo_cerv
+        AND cp.fk_cerv = it.fk_cerv_pres_1
+        AND cp.fk_pres = it.fk_cerv_pres_2
+        AND it.fk_cerv_pres_1 = dv.fk_inve_tien_1
+        AND it.fk_cerv_pres_2 = dv.fk_inve_tien_2
+        AND dv.fk_vent = v.cod_vent
+	GROUP BY tc.cod_tipo_cerv, tc.nombre_tipo_cerv, v.fecha_vent;
