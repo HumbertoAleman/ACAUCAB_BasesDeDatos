2eb62e73f3bbf14c8124ee45f5f73323f1f50035
Eiderwoss <oscarmanrique.sta@gmail.com>
2025-06-23 01:41:25 -0400

    fix: changed the type of t.credito using CAST

--- backend/src/ReportService.ts
+++ backend/src/ReportService.ts
@@ -309,7 +309,7 @@ class ReportService {
 					for (const row of data) {
 						let booleanInt = parseInt(row["Es de Crédito"]) ;
 						page.drawText(String(booleanInt), { x: 50, y, size: 10, font });
-						page.drawText(String(row["Cantidad"]), { x: 250, y, size: 10, font });
+						page.drawText(String(row["count"]), { x: 250, y, size: 10, font });
 						y -= 15;
 						if (y < 50){
 							page = pdfDoc.addPage([780, 800]);
