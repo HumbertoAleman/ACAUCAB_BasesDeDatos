78c2e8fb4b65bf41ddeae945fd2b59030e297143
Eiderwoss <oscarmanrique.sta@gmail.com>
2025-06-23 02:59:59 -0400

    feat: added drop for reportes_view

--- database/drop.sql
+++ database/drop.sql
@@ -138,3 +138,19 @@ DROP TABLE Miembro;
 
 -- DROP Lugar
 DROP TABLE Lugar;
+
+DROP VIEW tipo_cliente_view
+
+DROP VIEW horas_trabajo_view
+
+DROP VIEW productos_generan_repo_view
+
+DROP VIEW rentabilidad_tipo_view
+
+DROP VIEW rentabilidad_tipo_total_view
+
+DROP VIEW proporcion_tarjetas_view 
+
+DROP FUNCTION periodo_tipo_cliente
+
+DROP FUNCTION consolidar_horas
