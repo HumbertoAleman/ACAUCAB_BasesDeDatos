604fb49f26a7098d57117f06c0dc1ef919768db4
Eiderwoss <oscarmanrique.sta@gmail.com>
2025-06-22 21:11:33 -0400

    fix: problem with routes?

--- backend/index.ts
+++ backend/index.ts
@@ -10,7 +10,6 @@ import TasaService from "./src/TasaService";
 import VentaService, { type APIVenta } from "./src/VentaService";
 import AuthService from "./src/AuthService";
 import RolService from "./src/RolService";
-import InventoryService from "./src/InventoryService";
 
 console.log("Opening Backend on Port 3000");
 
@@ -34,10 +33,10 @@ Bun.serve({
 		"/api/form/parroquias": {
 			async GET() {
 				const res = await sql`
-				  SELECT p.cod_luga as eid, e.nombre_luga || ', ' || m.nombre_luga || ', ' || p.nombre_luga as "displayName"
-				  FROM Lugar AS e
-				  JOIN Lugar AS m ON e.cod_luga = m.fk_luga
-				  JOIN Lugar AS p ON m.cod_luga = p.fk_luga`
+					SELECT p.cod_luga as eid, e.nombre_luga || ', ' || m.nombre_luga || ', ' || p.nombre_luga as "displayName"
+					FROM Lugar AS e
+					JOIN Lugar AS m ON e.cod_luga = m.fk_luga
+					JOIN Lugar AS p ON m.cod_luga = p.fk_luga`
 				return Response.json(res, CORS_HEADERS);
 			},
 		},
@@ -79,10 +78,39 @@ Bun.serve({
 		...PrivilegesService.routes,
 		...ClientesService.routes,
 		...RolService.routes,
-		...InventoryService.routes,
-		...TasaService.routes,
-		...VentaService.routes,
 
+		"/api/tasa": {
+			OPTIONS: () => new Response('Departed', CORS_HEADERS),
+			async GET(req, _) {
+				const res = (await TasaService.getTasaDiaActual())
+				return Response.json(res, CORS_HEADERS)
+			}
+		},
 
+		"/api/venta": {
+			OPTIONS: () => new Response('Departed', CORS_HEADERS),
+			async POST(req, _) {
+				const res = await VentaService.registerVenta((await req.json()) as APIVenta)
+				return Response.json(res, CORS_HEADERS);
+			}
+		},
+
+		"/api/inventory": {
+			OPTIONS: () => new Response('Departed', CORS_HEADERS),
+			async GET() {
+				const res = await sql`SELECT C.nombre_cerv AS "nombre_producto", P.nombre_pres AS "nombre_presentacion", SUM(IT.cant_pres) AS "stock_actual", AVG(IT.precio_actual_pres)::numeric(8,2) AS "precio_usd", LT.nombre_luga_tien AS "lugar_tienda", M.razon_social_miem AS "miembro_proveedor"
+					FROM Inventario_Tienda AS IT
+					JOIN Cerveza AS C ON C.cod_cerv = IT.fk_cerv_pres_1
+					JOIN Presentacion AS P ON P.cod_pres = IT.fk_cerv_pres_2
+					JOIN CERV_PRES AS CP ON CP.fk_cerv = C.cod_cerv AND CP.fk_pres = P.cod_pres
+					JOIN Miembro AS M ON CP.fk_miem = M.rif_miem
+					JOIN Lugar_Tienda AS LT ON LT.cod_luga_tien = IT.fk_luga_tien
+					WHERE fk_tien = 1
+					GROUP BY C.nombre_cerv, P.nombre_pres, LT.nombre_luga_tien, M.razon_social_miem`
+				for (const item of res)
+					item.estado = (item.stock_actual > 100) ? "Disponible" : ((item.stock_actual === 0) ? "Agotado" : "Bajo Stock")
+				return Response.json(res, CORS_HEADERS);
+			}
+		}
 	}
 })
