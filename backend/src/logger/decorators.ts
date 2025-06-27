2165880b5ca35f2f6bd1d28ffc696e074a98294a
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-27 17:10:36 -0400

    fix + feat: Created decorator for exceptions + better logging for LogDecorator

--- backend/src/logger/decorators.ts
+++ backend/src/logger/decorators.ts
@@ -1,9 +1,24 @@
-function LogFunctionExecution(target: any, propertyName: string, descriptor: PropertyDescriptor): void {
-    const originalMethod = descriptor.value;
-    descriptor.value = function (...args: any[]) {
-        console.log(`Executing ${propertyName} with parameters:`, args);
-        return originalMethod.apply(this, args);
-    };
+import { CORS_HEADERS } from "../../globals";
+import Logger from "./logger";
+
+function LogFunctionExecution(_: any, propertyName: string, descriptor: PropertyDescriptor): void {
+	const originalMethod = descriptor.value;
+	descriptor.value = function (...args: any[]) {
+		Logger.debug(`Executing ${propertyName} with parameters: ${args}`);
+		return originalMethod.apply(this, args);
+	};
 }
 
-export { LogFunctionExecution }
+function sqlProtection(_: any, propertyName: string, descriptor: PropertyDescriptor): void {
+	const originalMethod = descriptor.value;
+	descriptor.value = async function (...args: any[]) {
+		try {
+			return await originalMethod.apply(this, args);
+		} catch (e) {
+			Logger.error(`Function ${propertyName} returned an error:\n\t${e}`)
+			return Response.json({ error_information: e }, { ...CORS_HEADERS, status: 500 })
+		}
+	};
+}
+
+export { LogFunctionExecution, sqlProtection }
