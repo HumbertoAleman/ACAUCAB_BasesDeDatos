d9405317b9b5fa5696d22db562e57f7e385b0685
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-27 16:51:25 -0400

    chore: Cleaned up several files

--- database/helpers/check_inserted_count.sql
+++
@@ -1,4 +0,0 @@
--- Source: https://www.w3resource.com/PostgreSQL/snippets/find-row-count-for-tables-postgresql.php
-SELECT relname AS table_name, n_live_tup AS row_count
-FROM pg_stat_user_tables
-ORDER BY table_name;
